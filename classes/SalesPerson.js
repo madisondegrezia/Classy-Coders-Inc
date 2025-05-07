4d87699c63f35b8ed80f2b1171c0c5777b1b6c76
Lauren Balch <llbalch@gmail.com>
2025-05-07 11:30:47 -0500

    created salesperson and software engineer classes and added getters and setters for each class

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,8 +1,20 @@
-class SalesPerson {
+require("./Employee.js")
 
+class SalesPerson extends Employee {
+    #totalSales
+    constructor(name, position, salary, isHired, clients, totalSales) {
+        super(name, position, salary, isHired);
+        this.clients = clients;
+        this.#totalSales = 0;
+    }
 
+    getSalesNumbers() {
+        return this.#totalSales;
+    }
 
-    
+    makeSale(amount) {
+        this.#totalSales += amount;
+    }
 }
 
 module.exports = {
--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,4 +1,18 @@
-class SoftwareEngineer {
+require("./Employee.js")
+
+class SoftwareEngineer extends Employee {
+    #programmingLanguages
+    constructor(name, position, salary, isHired, programmingLanguages) {
+        super(name, position, salary, isHired);
+        this.#programmingLanguages = programmingLanguages;
+    }
+    get programmingLanguages() {
+        return this.#programmingLanguages;
+    }
+
+    set programmingLanguages(language) {
+        this.#programmingLanguages = language;
+    }
 
 
 
